047c2a3af61ef3de74c562c8520f8b1be05a6e76
harshini2001 <54399043+harshini2001@users.noreply.github.com>
2020-05-14 12:01:49 +0530

    speech n text done

---
+++ Screens/audio1.py
@@ -0,0 +1,13 @@
+import speech_recognition as sr
+
+r=sr.Recognizer()
+
+with sr.Microphone() as source:
+    print('Speak Anyhting')
+    audio = r.listen(source)
+
+    try:
+        text=r.recognize_google(audio)
+        print('you say :{}',format(text))
+    except :
+        print('Sorry could nt recognize ur voice')
